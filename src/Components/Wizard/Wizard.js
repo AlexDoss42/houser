35a591a46cf71ea61d2eefe63db9950a94a48c0c
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-04-26 11:21:58 -0600

    Add Property Input Front End

--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -3,6 +3,24 @@ import { Link } from 'react-router-dom'
 
 
 export default class Wizard extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state ={
+      name: '',
+      address: '',
+      city: '',
+      zipcode: ''
+    }
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
 
   render() {
     return (
@@ -10,6 +28,37 @@ export default class Wizard extends Component {
       <div>
         <h1>YOU'RE A WIZARD HARRY</h1>
         <Link to='/'><button>Cancel</button></Link>
+        <div className='wizardInputs'>
+          
+          <input
+          type='text'
+          placeholder='Name of listing'
+          name = 'name'
+          onChange={e => this.handleChange(e)}
+          />
+           
+           <input
+          type='text'
+          placeholder='Street Address'
+          name = 'address'
+          onChange={e => this.handleChange(e)}
+          />
+           
+           <input
+          type='text'
+          placeholder='City'
+          name = 'city'
+          onChange={e => this.handleChange(e)}
+          />
+           
+           <input
+          type='text'
+          placeholder='Zipcode'
+          name = 'zipcode'
+          onChange={e => this.handleChange(e)}
+          />
+          
+        </div>
       </div>
     )
   }
